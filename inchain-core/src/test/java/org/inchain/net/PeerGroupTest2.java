e158606fe800bfc1231b30d70f9f050c23bc7a7e
kiss1987f4 <156755027@qq.com>
2016-12-06 17:10:17 +0800

    测试时不需要手动启动，Appkit有注解服务会自动启动

--- inchain-core/src/test/java/org/inchain/net/PeerGroupTest2.java
+++ inchain-core/src/test/java/org/inchain/net/PeerGroupTest2.java
@@ -1,9 +1,10 @@
 package org.inchain.net;
 
+import java.util.Random;
+
 import org.inchain.BaseTestCase;
 import org.inchain.kits.PeerKit;
-import org.inchain.network.NetworkParams;
-import org.inchain.transaction.Transaction;
+import org.inchain.message.PingMessage;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,24 +12,14 @@ public class PeerGroupTest2 extends BaseTestCase {
 
 	@Autowired
 	private PeerKit peerKit;
-	@Autowired
-	private NetworkParams network;
 	
 	@Test
 	public void test() {
 		
-//		SeedManager seedManager = new RemoteSeedManager();
-//		seedManager.add(new Seed(new InetSocketAddress("192.168.1.181", 6888)));
-
-//		SeedManager seedManager = new NodeSeedManager();
-//		seedManager.add(new Seed(new InetSocketAddress("127.0.0.1", 6881), true, 25000));
-		
-		peerKit.startSyn();
-
 		while(true) {
-			peerKit.broadcastTransaction(new Transaction(network));
 			try {
-				Thread.sleep(100000l);
+				Thread.sleep(8000l);
+				peerKit.broadcastMessage(new PingMessage(new Random().nextLong()));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
