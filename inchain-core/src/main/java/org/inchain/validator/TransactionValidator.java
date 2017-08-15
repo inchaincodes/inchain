8f9d34bb7e9b47ad6517014f0865fc604c719d41
zoro1985 <251556768@qq.com>
2017-08-15 16:43:46 +0800

    //修改bug

--- inchain-core/src/main/java/org/inchain/validator/TransactionValidator.java
+++ inchain-core/src/main/java/org/inchain/validator/TransactionValidator.java
@@ -312,12 +312,13 @@ public class TransactionValidator {
 				return validatorResult;
 			} else {
 				result.setFee(txInputFee.subtract(txOutputFee));
+			}
+			if(tx.getType() == Definition.TYPE_PAY && network.getBestBlockHeight() > 0) {
 				if(result.getFee().compareTo(Definition.MIN_PAY_FEE) < 0) {
 					result.setResult(false, "手续费至少为0.1个INS");
 					return validatorResult;
 				}
 			}
-
 			//业务交易且带代币交易
 			if(tx.getType() == Definition.TYPE_ANTIFAKE_CODE_MAKE) {
 				//如果是验证码生成交易，则验证产品是否存在
